4f92e11beee50dc844ccd94fe41c5605f818a646
Pranav18062004 <79793482+Pranav18062004@users.noreply.github.com>
2023-10-07 01:21:22 +0530

    Add files via upload

---
+++ number_triangle.cpp
@@ -0,0 +1,23 @@
+/*PRN - 22070123092
+Print Number Triaangle*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, j;
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 5; j++)
+        {
+            cout << j << " ";
+        }
+        cout << '\n';
+    }
+    return 0;
+}
+/*
+Output:
+
+*/
